6ce2f66e3381fc053d968302c0ae559f96f54a4a
Mawaddahr <1073266@hr.nl>
2024-11-06 12:13:10 +0100

    Placing comments + follow flake8 guidelines + change variable names (clients, inventories, item_groups, data_provider, notification_processor)

--- api/processors/notification_processor.py
+++ api/processors/notification_processor.py
@@ -1,18 +1,39 @@
 import threading
+# Threading allows to use background timers to repeatedly call a function
 
 NOTIFICATION_UPDATE_INTERVAL_SEC = 30
+# Sets the interval at which notifications are sent.
+# It sends notifications every 30 seconds
 
 _queue = ["Dummy message"]
+# This is a queue with notifications in it
+
 
 def push(notification):
+    # This function receives a notification as a parameter
     global _queue
     _queue.append(notification)
+    # It adds the notification to the queue
+
 
 def send():
+    # Check if the queue contains any notifications
     if len(_queue) > 0:
         notification = _queue.pop(0)
+        # It removes the first notification in the queue
+        # and assigns it to the variable 'notification'
         print(notification)
+        # It the notification that was popped
     threading.Timer(NOTIFICATION_UPDATE_INTERVAL_SEC, send).start()
+    # Sets a timer to call 'send()' again after the specified interval,
+    # which is 30 seconds
+
+    # It creates a timer and tells the timer to countdown,
+    # before calling the function again.
+
+    # After 30 seconds 'send()' is called again
+
 
 def start():
+    # Calls 'send()'
     send()
--- api/providers/data_provider.py
+++ api/providers/data_provider.py
@@ -1,3 +1,4 @@
+# Imports all classes from the files in the 'models' folder
 from models.warehouses import Warehouses
 from models.locations import Locations
 from models.transfers import Transfers
@@ -11,10 +12,13 @@ from models.orders import Orders
 from models.clients import Clients
 from models.shipments import Shipments
 
+# constant boolean set to False
 DEBUG = False
 
+# Constant string.
 ROOT_PATH = "./data/"
 
+# Create variables. These are assigned to none.
 _warehouses = None
 _locations = None
 _transfers = None
@@ -30,6 +34,11 @@ _clients = None
 
 
 def init():
+    # create global variables that can be used within functions.
+
+    # Functions can give it other values,
+    # however then it would be treated as a local variable in that function
+    # Meaning for the rest of the program the value stays the same.
     global _warehouses
     _warehouses = Warehouses(ROOT_PATH, DEBUG)
     global _locations
@@ -55,50 +64,65 @@ def init():
     global _shipments
     _shipments = Shipments(ROOT_PATH, DEBUG)
 
+# With pool they mean that this object contains functions,
+# that can retrieve, update, delete, add data to the Warehouse database.
+
 
 def fetch_warehouse_pool():
+    # This method returns an object of the class Warehouses.
     return _warehouses
 
 
 def fetch_location_pool():
+    # Returns an object of type Locations
     return _locations
 
 
 def fetch_transfer_pool():
+    # Returns an object of type Transfers
     return _transfers
 
 
 def fetch_item_pool():
+    # Returns and object of type Items
     return _items
 
 
 def fetch_item_line_pool():
+    # Returns an object of type ItemLines
     return _item_lines
 
 
 def fetch_item_group_pool():
+    # Returns an object of type ItemGroups
     return _item_groups
 
 
 def fetch_item_type_pool():
+    # Returns an object of type ItemTypes
     return _item_types
 
 
 def fetch_inventory_pool():
+    # Returns an object of type Inventories
     return _inventories
 
 
 def fetch_supplier_pool():
+    # Returns an object of type Suppliers
     return _suppliers
 
 
 def fetch_order_pool():
+    # Returns an object of type Orders
     return _orders
 
 
 def fetch_client_pool():
+    # Returns an object of type Clients
     return _clients
 
 
 def fetch_shipment_pool():
+    # Returns an object of type Shipments
     return _shipments
