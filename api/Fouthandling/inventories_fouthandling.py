589aa7dc57e8d3e2ec8155d87e88b39fa1efc832
Mawaddahr <1073266@hr.nl>
2024-11-07 11:14:05 +0100

    Basis fouthandling voor de bestanden clients, inventories en itemgroups. Implementeren moet nog

---
+++ api/Fouthandling/inventories_fouthandling.py
@@ -0,0 +1,67 @@
+from models.inventories import Inventories
+
+
+class InventoriesFoutHandling:
+
+    def __init__(self):
+        self.inventories = Inventories("", True)
+        self.RequiredFields = [
+            "id", "item_id", "description",
+            "item_reference", "locations", "total_on_hand",
+            "total_expected", "total_ordered", "total_allocated",
+            "total_available"
+        ]
+
+    def check_valid_id(self, inventory_id):
+        # checks on negatieve ids
+        if inventory_id < 0:
+            return False
+        return True
+
+    def check_valid_body(self, inventory):
+        # checks on valid json body
+        # checks if body has all required fields
+        for field in self.RequiredFields:
+            if field not in inventory:
+                return False
+        return True
+
+    def check_get_inventory(self, inventory_id):
+        # Checks on negatieve ids
+        return self.check_valid_id(inventory_id)
+
+    def check_get_inventory_for_item(self, item_id):
+        # Checks valid item id
+        # Ik ga hier en in de methode eronder probs checken of
+        # het in database staat.
+        # Daarna call ik dit gewoon in de inventories.py
+        for inventory in self.inventories.inventory_database:
+            if inventory["item_id"] == item_id:
+                return True
+        return False
+
+    def check_get_inventory_totals_for_item(self, item_id):
+        # checks valid item id?
+        # Anything else maybe too?
+        return self.check_get_inventory_for_item(item_id)
+
+    def check_add_inventory(self, inventory):
+        # checks valid body
+        if self.check_valid_body(inventory):
+            # checks if id not in database
+            for inventaris in self.inventories.inventory_database:
+                if inventaris["id"] == inventory["id"]:
+                    return False
+        return True
+
+    def check_put_inventory(self, inventory, inventory_id):
+        # checks valid body
+        if self.check_valid_body(self, inventory):
+            # checks if id body == id parameter
+            if inventory["id"] != inventory_id:
+                return False
+        return True
+
+    def check_remove_inventory(self, inventory_id):
+        # Check on valid Id
+        return self.check_valid_id(inventory_id)
---
+++ api/Fouthandling/itemgroups_fouthandling.py
@@ -0,0 +1,50 @@
+from models.item_groups import ItemGroups
+
+'''
+{"id": 0,
+"name": "Electronics",
+"description": "",
+'''
+
+
+class ItemGroupsFoutHandling:
+
+    def __init__(self):
+        self.itemgroups = ItemGroups("", True)
+        self.RequiredFields = [
+            "id", "name", "description"
+        ]
+
+    def check_valid_id(self, itemgroup_id):
+        # checks on negative ids
+        if itemgroup_id < 0:
+            return False
+        return True
+
+    def check_valid_body(self, itemgroup):
+        # checks if all required fields in body
+        for field in self.RequiredFields:
+            if field not in itemgroup:
+                return False
+        return True
+
+    def check_get_itemgroup(self, itemgroup_id):
+        return self.check_valid_id(itemgroup_id)
+
+    def check_add_itemgroup(self, itemgroup):
+        if self.check_valid_body(itemgroup):
+            # check if id not in database
+            for itemgroep in self.itemgroups.item_groups_database:
+                if itemgroep["id"] == itemgroup["id"]:
+                    return False
+        return True
+
+    def check_put_itemgroup(self, itemgroup, itemgroup_id):
+        if self.check_valid_body(itemgroup):
+            # check if itemgroup_id is the same as itemgroup["id"]
+            if itemgroup["id"] != itemgroup_id:
+                return False
+        return True
+
+    def check_remove_itemgroup(self, itemgroup_id):
+        return self.check_valid_id(itemgroup_id)
